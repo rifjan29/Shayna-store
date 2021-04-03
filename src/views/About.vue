c08a60772f2bed9af719bb8dd34d4ecd1f66f741
rifjan29 <rjndla@gmail.com>
2021-04-04 00:53:31 +0800

    Component:Header, Carousel, Footer

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <carousel>
+
+    <img src="https://placeimg.com/200/200/any?1">
+
+    <img src="https://placeimg.com/200/200/any?2">
+
+    <img src="https://placeimg.com/200/200/any?3">
+
+    <img src="https://placeimg.com/200/200/any?4">
+
+  </carousel>
+  </div>
+</template>
+
+<script>
+
+import carousel from 'vue-owl-carousel'
+
+export default {
+    components: { carousel },
+}
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <header-shayna/>
+    <hero-shayna/>
+    <content-shayna/>
+    <footer-shayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import ContentShayna from '@/components/ContentShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    ContentShayna,
+    FooterShayna
+    // HelloWorld
+  },
+  data(){
+    return{
+      member:{
+        name: 'try',
+        coverImg: "img/hero-2.jpg"
+      }
+    }
+  }
+}
+</script>
